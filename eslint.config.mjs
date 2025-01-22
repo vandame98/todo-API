1d96cd16e53827310238e9ae00c33ddb72134d40
Vandame Hrustic Amirovic <vandame@mobilemediacontent.com>
2025-01-22 23:22:21 +0100

    chore(prettier): Configure prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,25 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest"
+    },
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"],
+      "eqeqeq": "error",
+      "no-console": "warn",
+      "indent": ["error", 2]
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  prettier
 ];
