d1c5f35cae82a7d9b2f82ee97629f8adf3f56f82
Vandame Hrustic Amirovic <vandame@mobilemediacontent.com>
2025-01-26 13:22:11 +0100

    chore(middlewares): configure middlewares with cors express and helmet

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import { setupSwagger } from './config/swagger';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app: Express = express();
+app.use(express.json()); // Converts the req.body to JSON
+app.use(express.urlencoded({ extended: true })); // Allow to send encoded data with `x-www-form-urlencoded`
+app.use(cors()); // Allow requests from other domains
+app.use(helmet()); // Add security to HTTP headers
+
 setupSwagger(app);
 app.get('/', (req: Request, res: Response) => {
   res.send('✅ Hello World from Express');
